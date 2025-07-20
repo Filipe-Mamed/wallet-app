f7422d024cfe3d1d5d65d8a79e35a107982da9ae
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-20 03:24:09 -0300

    refactor: exportando componente (Button)

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./ButtonSocialGoogle/ButtonSocialGoogle"
 export * from "./ButtonSocialFacebook/ButtonSocialFacebook"
 export * from "./Input_old/Input"
+export * from "./Button/Button"
