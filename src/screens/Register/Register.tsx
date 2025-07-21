709925f7c198e008659ea9a4a697f6ab7eef93f1
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-20 23:24:23 -0300

    feat: Crianção do componente (Register)

---
+++ src/screens/Register/Register.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { styles } from "./Register.Styled"
+
+export function Register(){
+    return(
+        <View style={styles.container}>
+            <Text>Cadastrar</Text>
+        </View>
+    )
+}
