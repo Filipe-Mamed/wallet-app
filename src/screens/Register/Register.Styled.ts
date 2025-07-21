6322f76a928be64f813297de8f2effc763bd0344
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-21 03:11:04 -0300

    feat: Criando a screen de (Register)

--- src/screens/Register/Register.Styled.ts
+++ src/screens/Register/Register.Styled.ts
@@ -1,7 +1,57 @@
 import { StyleSheet } from "react-native";
+import { Fonts } from "../../shared/styles";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const styles = StyleSheet.create({
-    container:{
-        backgroundColor: "#ccc"
-    }
-})
+  container: {
+    backgroundColor: "#F8F8FF",
+    padding: RFValue(13),
+  },
+  header: {
+    padding: RFValue(20),
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "black",
+    fontFamily: Fonts.Poppins_Medium,
+    marginTop: RFValue(40),
+    fontSize: RFValue(25),
+    textAlign: "center",
+  },
+  description: {
+    color: "black",
+    fontFamily: Fonts.Poppins_Light,
+    marginTop: RFValue(40),
+    fontSize: RFValue(15),
+  },
+  main: {
+    gap: RFValue(15),
+    marginTop: RFValue(20),
+  },
+  buttonEntrar: {
+    marginTop: RFValue(40),
+    gap: RFValue(10),
+  },
+  footer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: RFValue(5),
+    marginTop: RFValue(25),
+  },
+  textFooter: {
+    fontSize: RFValue(16),
+    fontFamily: Fonts.Poppins_Regular,
+    color: "#808080",
+  },
+  buttonFooter: {
+    width: "auto",
+    fontSize: RFValue(16),
+    fontFamily: Fonts.Poppins_Regular,
+    textDecorationLine: "underline",
+    color: "#0000FF",
+    textDecorationColor: "#0000FF",
+  },
+});
--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,10 +1,76 @@
-import { Text, View } from "react-native"
-import { styles } from "./Register.Styled"
+import {
+  Text,
+  View,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native";
+import { styles } from "./Register.Styled";
+import { Button } from "../../shared/components";
+import { Input } from "../../shared/components/Input/Input";
+import { useNavigation } from "@react-navigation/native";
 
-export function Register(){
-    return(
+export function Register() {
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    navigation.navigate("Login");
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
-            <Text>Cadastrar</Text>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              Ao se registrar, sinta imediatamente a facilidade de realizar
+              transações.
+            </Text>
+            <Text style={styles.description}>Cadastre-se</Text>
+          </View>
+          <View style={styles.main}>
+            <Input
+              name="nome"
+              placeholder="Nome"
+              keyboardType="default"
+              autoCapitalize="none"
+              autoCorrect={false}
+              leftImg={require("../../assets/imgs/InputsImgs/do-utilizador.png")}
+            />
+            <Input
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              leftImg={require("../../assets/imgs/InputsImgs/e-mail.png")}
+            />
+            <Input
+              name="senha"
+              placeholder="Senha"
+              keyboardType="default"
+              autoCapitalize="none"
+              autoCorrect={false}
+              showPasswordToggle
+              leftImg={require("../../assets/imgs/InputsImgs/trancar.png")}
+              rightImg={require("../../assets/imgs/InputsImgs/olho-aberto.png")}
+            />
+          </View>
+          <View style={styles.buttonEntrar}>
+            <Button variant="purple" children="Cadastrar" onPress={() => {}} />
+          </View>
+          <View style={styles.footer}>
+            <Text style={styles.textFooter}>Já tem cadastro?</Text>
+            <Button
+              style={styles.buttonFooter}
+              variant="transparent"
+              children="Entrar"
+              onPress={handleLogin}
+            />
+          </View>
         </View>
-    )
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
