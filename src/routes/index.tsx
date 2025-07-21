bfd20f98e7c8f3296f776168c86cf82466eb966f
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-21 00:37:34 -0300

    refactor: Exportação de rotas em index.ts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import { Text, View } from "react-native";
-
-import { TabRoutes } from "./TabRoutes";
-import { AppRoutes } from "./AppRoutes";
-import { AuthRoutes } from "./AuthRoutes";
-
-export function Routes() {
-  return <AuthRoutes/>
-}
+export * from "./AppRoutes"
