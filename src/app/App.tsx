49632ad4a4723788e848ccea23b1cd18e5bafa00
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-21 03:16:54 -0300

    style: Adicionando backgroundColor em SafeArearView

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,19 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { FontProvider } from "../shared/contexts";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {NavigationContainer} from "@react-navigation/native"
-import {AppRoutes} from "../routes"
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "../routes";
 export default function App() {
   return (
     <NavigationContainer>
       <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <FontProvider>
-          <StatusBar style="auto" translucent />
-          <AppRoutes/>
-        </FontProvider>
-      </SafeAreaView>
-    </GestureHandlerRootView>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#F8F8FF" }}>
+          <FontProvider>
+            <StatusBar style="auto" translucent />
+            <AppRoutes />
+          </FontProvider>
+        </SafeAreaView>
+      </GestureHandlerRootView>
     </NavigationContainer>
   );
 }
