312e3f496afed0073c82a52c9acdfb1eb11748f2
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-20 23:25:00 -0300

    feat: Criação do componente (RecoverPassword)

---
+++ src/screens/RecoverPassword/RecoverPassword.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { styles } from "./RecoverPassword.Styled"
+
+export function RecoverPassword(){
+    return(
+        <View style={styles.container}>
+            <Text>Recuperar senha</Text>
+        </View>
+    )
+}
