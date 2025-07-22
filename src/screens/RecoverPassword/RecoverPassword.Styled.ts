e731c0f75ef1b00b36ea563084de16eaaf95396a
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-21 22:41:30 -0300

    feat: Criando a screen de (RecoverPassword)

--- src/screens/RecoverPassword/RecoverPassword.Styled.ts
+++ src/screens/RecoverPassword/RecoverPassword.Styled.ts
@@ -1,7 +1,60 @@
 import { StyleSheet } from "react-native";
+import { Fonts } from "../../shared/styles";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export const styles = StyleSheet.create({
-    container:{
-        backgroundColor: "#000000"
-    }
-})
+const styles = StyleSheet.create({
+  Container: {
+    backgroundColor: "#F8F8FF",
+    padding: RFValue(13),
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  Header: {
+    marginTop: RFValue(50),
+    padding: RFValue(20),
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  Title: {
+    color: "black",
+    fontFamily: Fonts.Poppins_Medium,
+    marginTop: RFValue(40),
+    fontSize: RFValue(25),
+    textAlign: "center",
+  },
+  Description: {
+    color: "black",
+    fontFamily: Fonts.Poppins_Light,
+    marginTop: RFValue(40),
+    fontSize: RFValue(15),
+    textAlign: "center"
+  },
+  Body: {
+    marginTop: RFValue(20),
+    gap:RFValue(25),
+    width: "100%",
+  },
+  Footer:{
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: RFValue(5),
+    marginTop: RFValue(40),
+    marginBottom: RFValue(180),
+  },
+  TextFooter:{
+    fontSize: RFValue(16),
+    fontFamily: Fonts.Poppins_Regular,
+    color: "#808080",
+  },
+  ButtonFooter:{
+    width: "auto",
+    fontSize: RFValue(16),
+    fontFamily: Fonts.Poppins_Regular,
+    textDecorationLine: "underline",
+    color: "#0000FF",
+    textDecorationColor: "#0000FF",
+  }
+});
+
+export const { Container, Title, Description, Header, Body, Footer, TextFooter, ButtonFooter } = styles;
--- src/screens/RecoverPassword/RecoverPassword.tsx
+++ src/screens/RecoverPassword/RecoverPassword.tsx
@@ -1,10 +1,63 @@
-import { Text, View } from "react-native"
-import { styles } from "./RecoverPassword.Styled"
+import {
+  Text,
+  View,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from "react-native";
+import {
+  Container,
+  Header,
+  Title,
+  Description,
+  Body,
+  Footer,
+  TextFooter,
+  ButtonFooter,
+} from "./RecoverPassword.Styled";
+import { Input } from "../../shared/components/Input/Input";
+import { Button } from "../../shared/components";
+import { useNavigation } from "@react-navigation/native";
 
-export function RecoverPassword(){
-    return(
-        <View style={styles.container}>
-            <Text>Recuperar senha</Text>
+export function RecoverPassword() {
+  const navigation = useNavigation();
+
+  const handleBackLogin = () => {
+    navigation.navigate("Login");
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={Container}>
+          <View style={Header}>
+            <Text style={Title}>Esqueceu sua senha?</Text>
+            <Text style={Description}>
+              Informe o e-mail cadastrado para redefinir sua senha.
+            </Text>
+          </View>
+          <View style={Body}>
+            <Input
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              leftImg={require("../../assets/imgs/InputsImgs/e-mail.png")}
+            />
+            <Button variant="purple" children="Enviar" onPress={() => {}} />
+          </View>
+          <View style={Footer}>
+            <Text style={TextFooter}>Lembrou a senha?</Text>
+            <Button
+              style={ButtonFooter}
+              variant="transparent"
+              children="Fazer login"
+              onPress={handleBackLogin}
+            />
+          </View>
         </View>
-    )
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
