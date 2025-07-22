b45b71ecd59db5cb611c5acf86e6e9fe9ebfbfca
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-21 22:42:34 -0300

    feat: Passando Props onPress no componente (ForgotPassword)

--- src/screens/Login/components/ForgotPassword/index.tsx
+++ src/screens/Login/components/ForgotPassword/index.tsx
@@ -2,14 +2,18 @@ import { Text, View } from "react-native";
 import { Button } from "../../../../shared/components";
 import { styles } from "./ForgotPassword.Styled";
 
-export function ForgotPassword() {
+interface IForgotProps {
+  onPress: () => void;
+}
+
+export function ForgotPassword({onPress}: IForgotProps) {
   return (
     <>
       <Text style={styles.textForgotPassword}>Esqueceu a senha?</Text>
       <Button
         variant="transparent"
         children="Redefinir senha"
-        onPress={() => {}}
+        onPress={onPress}
         style={styles.buttonForgotPassword}
       />
     </>
