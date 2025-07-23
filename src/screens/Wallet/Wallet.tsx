bd70ae1488191a22c3bfbe2b6c301f452cd7a296
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-23 18:33:47 -0300

    feat: Adicionando componentes

--- src/screens/Wallet/Wallet.tsx
+++ src/screens/Wallet/Wallet.tsx
@@ -5,15 +5,32 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
 } from "react-native";
-import { $container, $header, $text } from "./Wallet.Styled";
+import { $container, $contentCard, $body } from "./Wallet.Styled";
+import { Header } from "../../shared/components";
+import { CardWallet } from "./components/CardWallet";
+import { FinanceIcons } from "./components/FinanceIcons";
+
+import FotoIlustrativa from "../../assets/imgs/foto-ilustrativa.jpg";
 
 export function Wallet() {
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={$container}>
-          <View style={$header}>
-            <Text style={$text}>Wallet</Text>
+          <Header
+            name="Carteira"
+            status="Ativo"
+            // imgBack={{
+            //   source: require("../../assets/imgs/botão-voltar.png"),
+            //   style: { tintColor: "#4B0082", width: 43, height: 43 },
+            // }}
+            avatar={FotoIlustrativa}
+          />
+          <View style={$contentCard}>
+            <CardWallet />
+          </View>
+          <View style={$body}>
+            <FinanceIcons />
           </View>
         </View>
       </TouchableWithoutFeedback>
