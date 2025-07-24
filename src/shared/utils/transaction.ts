c838c977285761c706f07b47eb4c9d66b0293a7e
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-23 23:36:52 -0300

    feat: Criação de um Banco de dados fictício

---
+++ src/shared/utils/transaction.ts
@@ -0,0 +1,48 @@
+import Netflix from "../../assets/imgs/TransactionImgs/netflix.png";
+import Amazon from "../../assets/imgs/TransactionImgs/amazon.png";
+import Paypal from "../../assets/imgs/TransactionImgs/paypal.png";
+
+export const transaction = [
+  {
+    id: 1,
+    title: "Netflix",
+    subTitle: "Assinatura Mensal",
+    price: 49.99,
+    imgIcon: Netflix,
+  },
+  {
+    id: 2,
+    title: "Amazon",
+    subTitle: "Amazon Prime",
+    price: 100.0,
+    imgIcon: Amazon,
+  },
+  {
+    id: 3,
+    title: "Paypal",
+    subTitle: "Taxa de Serviço",
+    price: 10.50,
+    imgIcon: Paypal,
+  },
+  {
+    id: 4,
+    title: "Amazon",
+    subTitle: "Amazon Music",
+    price: 14.99,
+    imgIcon: Amazon,
+  },
+  {
+    id: 5,
+    title: "Paypal",
+    subTitle: "Pagto Fatura Cartão de Crédito",
+    price: 1500.55,
+    imgIcon: Paypal,
+  },
+  {
+    id: 6,
+    title: "Netflix",
+    subTitle: "Plano Adquirido",
+    price: 99.99,
+    imgIcon: Netflix,
+  },
+];
