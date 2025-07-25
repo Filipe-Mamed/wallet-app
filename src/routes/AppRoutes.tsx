98d0ef9d07b3a33a0d09ebd9e7a5e490feb2a6c6
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-25 01:58:33 -0300

    feat: Adicionado ScreenRoutes em AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,13 +3,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { AuthRoutes } from "./AuthRoutes";
 import { TabRoutes } from "./TabRoutes";
+import {ScreenRoutes} from "./ScreenRoutes"
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator initialRouteName="Screen" screenOptions={{ headerShown: false }}>
       {/* <Screen name="Auth" component={AuthRoutes} /> */}
+
+      <Screen name="Screen" component={ScreenRoutes} />
       <Screen name="Main" component={TabRoutes} />
     </Navigator>
   );
