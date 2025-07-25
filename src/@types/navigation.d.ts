ad691700f53c4d89f16460eaa63aae7072e78b81
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-25 01:48:31 -0300

    feat: Adicionado novas declarações de rotas globais

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,9 +1,20 @@
+interface ScreenRoutesParamList{
+  Profile: undefined
+}
+
 export declare global {
-    namespace ReactNavigation {
-        interface RootParamList {
-            Login: undefined
-            Register: undefined
-            RecoverPassword: undefined
-        }
+  namespace ReactNavigation {
+    interface RootParamList {
+      // Rotas que precisam de autentificação
+      Login: undefined;
+      Register: undefined;
+      RecoverPassword: undefined;
+
+      // Telas
+      Screen:{
+        screen: keyof ScreenRoutesParamList
+        params?: ScreenRoutesParamList[keyof ScreenRoutesParamList];
+      }
     }
+  }
 }
