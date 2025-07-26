0b45f397837d910dc50397eefa74e519214816c3
Filipe-Mamed <filipemamed9@gmail.com>
2025-07-26 03:15:58 -0300

    feat: Adicionando a estrutura de (Profile)

--- src/screens/Profile/Profile.Styled.ts
+++ src/screens/Profile/Profile.Styled.ts
@@ -7,6 +7,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F8F8FF",
   },
+  $avatar: {
+    width: RFValue(95),
+    height: RFValue(95),
+    borderRadius: RFValue(50),
+    alignSelf: "center",
+  },
+  $contentUser: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: RFValue(20),
+    marginLeft: RFValue(30),
+  },
+  $nameUser: {
+    fontSize: RFValue(20),
+    fontFamily: Fonts.Poppins_Bold,
+  },
+  $imgEdit:{
+    width: RFValue(22),
+    height: RFValue(22),
+    tintColor: "#45197D",
+    marginLeft: RFValue(10),
+  },
+  $body: {
+    paddingHorizontal: RFValue(20),
+    paddingTop: RFValue(40),
+    gap: RFValue(40),
+  },
+  $footer: {
+    marginTop: RFValue(70),
+    gap: RFValue(10),
+  },
 });
 
-export const { $container } = styles;
+export const { $container, $avatar, $contentUser, $nameUser, $imgEdit, $body, $footer } =
+  styles;
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,18 +1,44 @@
-import { Text, View } from "react-native";
-import { $container } from "./Profile.Styled";
-import { Header } from "../../shared/components";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  $container,
+  $avatar,
+  $contentUser,
+  $nameUser,
+  $imgEdit,
+  $body,
+  $footer,
+} from "./Profile.Styled";
+import { ButtonSettings, Header, SettingsC } from "../../shared/components";
 
 import Back from "../../assets/imgs/botão-voltar.png";
+import Avatar from "../../assets/imgs/foto-ilustrativa.jpg";
+import Edit from "../../assets/imgs/editar.png";
 
 export function Profile() {
-
   return (
     <View style={$container}>
       <Header
         imgBack={{ source: Back, style: { tintColor: "#45197D" } }}
         nameRight="Perfil"
       />
-      <Text style={{textAlign: "center", marginTop: 300}}>Profile</Text>
+      <View>
+        <Image source={Avatar} resizeMode="cover" style={$avatar} />
+        <View style={$contentUser}>
+          <Text style={$nameUser}>Fulano</Text>
+          <TouchableOpacity activeOpacity={0.3}>
+            <Image source={Edit} style={$imgEdit} resizeMode="contain" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={$body}>
+        <SettingsC nameText="Conta conectada" rightArrow />
+        <SettingsC nameText="Privacidade e segurança" rightArrow />
+        <SettingsC nameText="Configurações de login" rightArrow />
+        <SettingsC nameText="Central de serviço" rightArrow />
+      </View>
+      <View style={$footer}>
+        <ButtonSettings button typeTrash text="Deletar Conta" />
+      </View>
     </View>
   );
 }
